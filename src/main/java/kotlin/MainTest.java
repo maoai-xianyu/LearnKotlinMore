a030528200cedfded3e7528c858b7b92dc34be43
zhangkun <zhangkun35@maoyan.com>
2020-11-25 11:15:06 +0800

    测试

--- src/main/java/kotlin/MainTest.java
+++ src/main/java/kotlin/MainTest.java
@@ -1,10 +1,16 @@
+package kotlin;
+
 /**
  * @author zhangkun
  * @time 2020/10/27 9:29 下午
  */
-public class Main {
+public class MainTest {
 
     public static void main(String[] args) {
         System.out.println("ssss");
+        String mm = "sdsdsdsdsdsd";
+
+        int i = mm.indexOf(null);
+        System.out.println(i);
     }
 }
