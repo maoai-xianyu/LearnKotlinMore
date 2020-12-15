95f76bb99c27fe14a5f76f4067f0fa313bd2b877
zhangkun <zhangkun35@maoyan.com>
2020-12-15 21:52:04 +0800

    copy

--- src/main/java/kotlin/chapter10/TestList.java
+++ src/main/java/kotlin/chapter10/TestList.java
@@ -1,15 +1,27 @@
 package kotlin.chapter10;
 
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.collections4.CollectionUtils;
+
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class TestList {
 
     public static void main(String[] args) {
+        modelCopy();
+        coll();
+        collDeep();
+        collBeautiful();
+
+    }
 
 
+    private static void modelCopy() {
         ArrayList<P> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             list.add(new P(i, "i " + i));
@@ -33,7 +45,7 @@ public class TestList {
         for (int i = 0; i < listNew.size(); i++) {
             if (listNew.get(i).id == 1) {
                 P p1 = listNew.get(i);
-                p1.name="zhang";
+                p1.name = "zhang";
                 break;
             }
         }
@@ -47,22 +59,87 @@ public class TestList {
         for (P p : listNew) {
             System.out.println(p);
         }
+    }
+
+    private static void coll() {
+        System.out.println("CollectionUtils -------- 浅拷贝");
+
+        List<PP> listP = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            listP.add(new PP(i, "i " + i));
+        }
+
+        //list深度拷贝
+        List<PP> newListP = new ArrayList<>();
+        CollectionUtils.addAll(newListP, listP);
+        Collections.copy(newListP, listP);
+        for (int i = 0; i < newListP.size(); i++) {
+            if (i == 1) {
+                newListP.get(i).name = "zhang";
+                break;
+            }
+        }
+
+        System.out.println("原list值：" + listP);
+        System.out.println("新list值：" + newListP);
+    }
+
+
+    private static void collDeep() {
+        System.out.println("DeepCopyUtil -------- 深拷贝 需要实体类继承 Serializable");
+
+        List<PP> listP = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            listP.add(new PP(i, "i " + i));
+        }
+
+        //list深度拷贝
+        List<PP> newListP = DeepCopyUtil.deepCopy(listP);
+        if (newListP != null) {
+            for (int i = 0; i < newListP.size(); i++) {
+                if (i == 1) {
+                    newListP.get(i).name = "zhang";
+                    break;
+                }
+            }
+
+            System.out.println("原list值：" + listP);
+            System.out.println("新list值：" + newListP);
+        }
 
     }
 
-    public static <T> List<T> deepCopy(List<T> src) throws IOException, ClassNotFoundException {
-        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-        ObjectOutputStream out = new ObjectOutputStream(byteOut);
-        out.writeObject(src);
+    private static void collBeautiful() {
+        System.out.println("collBeautiful -------- 深拷贝 需要实体类继承 Serializable");
 
-        ByteArrayInputStream byteIn = new ByteArrayInputStream(byteOut.toByteArray());
-        ObjectInputStream in = new ObjectInputStream(byteIn);
-        @SuppressWarnings("unchecked")
-        List<T> dest = (List<T>) in.readObject();
-        return dest;
+        List<PP> listP = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            listP.add(new PP(i, "i " + i));
+        }
+
+        //list深度拷贝
+        try {
+            List<PP> newListP = new ArrayList<>();
+            for (int i = 0; i < listP.size(); i++) {
+                newListP.add((PP) BeanUtils.cloneBean(listP.get(i)));
+            }
+
+            for (int i = 0; i < newListP.size(); i++) {
+                if (i == 1) {
+                    newListP.get(i).name = "zhang";
+                }
+            }
+
+            System.out.println("原list值：" + listP);
+            System.out.println("新list值：" + newListP);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
-    static class P implements Cloneable{
+
+    static class P implements Cloneable {
 
         public P(int id, String name) {
             this.id = id;
@@ -85,4 +162,26 @@ public class TestList {
             return (P) super.clone();
         }
     }
+
+
+    static class PP implements Serializable {
+
+        public PP(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        int id;
+        String name;
+
+        @Override
+        public String toString() {
+            return "P{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+
+    }
+
 }
